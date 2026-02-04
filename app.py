f871f4f6ab4106d7d46f798e1da2279076fbd004
Filippo <filruf1702@gmail.com>
2026-02-04 12:47:46 -0500

    conflitto main

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ import threading
 
 
 OUTPUT_DIR = "outputs"
+OUTPUT_DIR = "outputs"
 MODEL_ROOT = os.path.join("models", "VLM")
 PRIVATE_TOKEN = os.getenv("VLM_PRIVATE_TOKEN", "")
 HF_TOKEN = os.getenv("HUGGINGFACE_HUB_TOKEN") or os.getenv("HF_TOKEN")
