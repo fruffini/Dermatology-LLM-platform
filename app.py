607dcb4d736e17bd6060402fd92e6edba6727309
Filippo <filruf1702@gmail.com>
2026-02-04 12:56:44 -0500

    correction to prova, back as before

--- app.py
+++ app.py
@@ -19,19 +19,10 @@ from transformers import (
 )
 import threading
 
-<<<<<<< HEAD
-
-
-
-
-OUTPUT_DIR = "outputs"
-OUTPUT_DIR = "outputs"
-=======
 OUTPUT_DIR = "outputs/baseline_vlm_ramo_prova_conflitto"
 var = os.getenv("OUTPUT_DIR")
 if var:
     OUTPUT_DIR = var
->>>>>>> 5205ce5 (correction to prova, back as before)
 MODEL_ROOT = os.path.join("models", "VLM")
 PRIVATE_TOKEN = os.getenv("VLM_PRIVATE_TOKEN", "")
 HF_TOKEN = os.getenv("HUGGINGFACE_HUB_TOKEN") or os.getenv("HF_TOKEN")
